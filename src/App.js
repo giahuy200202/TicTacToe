60ca4fb63c77bf5617d670948f05f348b63a784f
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2023-10-21 15:34:23 +0700

    highlight the path for winner and take position history

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import $ from "jquery";
 
-function Square({ value, onSquareClick }) {
+function Square({ value, indColor, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={`square ${indColor}`} onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -10,7 +11,8 @@ function Square({ value, onSquareClick }) {
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
+    const objWinner = calculateWinner(squares);
+    if (objWinner.winner !==null || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -22,30 +24,44 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares, i, nextSquares[i]);
   }
 
-  const winner = calculateWinner(squares);
+  const objWinner = calculateWinner(squares);
   let status;
-  if (winner) {
-    status = "Winner: " + winner;
+  if (objWinner.winner!==null) {
+    status = "Winner: " + objWinner.winner;
+    for(let i=0; i<objWinner.path.length; i++){
+      console.log(`.square ${objWinner.path[i]}`);
+      $(`.${objWinner.path[i]}`).css('background-color', 'yellow');
+    }
+   
   } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    $(`.square`).css('background-color', '#fff');
+    if(!squares.includes(null)){
+      status = "Draw";
+    }
+    else{
+      status = "Next player: " + (xIsNext ? "X" : "O");
+    }
   }
+    return (
+      <>
+        <div className="status">{status}</div>
+        {[0, 3, 6].map((row, indexRow) => (
+          <div className="board-row" key={indexRow}>
+            {[0, 1, 2].map((column, indexColumn) => (
+              <Square
+                // id={`${row + column}`}
+                className="abc"
+                key={indexColumn}
+                value={squares[row + column]}
+                indColor = {row+column}
+                onSquareClick={() => handleClick(row + column)}
+              />
+            ))}
+          </div>
+        ))}
+      </>
+    );
 
-  return (
-    <>
-      <div className="status">{status}</div>
-      {[0, 3, 6].map((row, indexRow) => (
-        <div className="board-row" key={indexRow}>
-          {[0, 1, 2].map((column, indexColumn) => (
-            <Square
-              key={indexColumn}
-              value={squares[row + column]}
-              onSquareClick={() => handleClick(row + column)}
-            />
-          ))}
-        </div>
-      ))}
-    </>
-  );
 }
 
 export default function Game() {
@@ -78,7 +94,7 @@ export default function Game() {
     let description;
     let moreInfoPositionAndPlayer;
     if (move > 0) {
-      moreInfoPositionAndPlayer= `at position (${parseInt(
+      moreInfoPositionAndPlayer = `at position (${parseInt(
         positionWithPlayerHistory[move - 1].index / 3
       )}, ${positionWithPlayerHistory[move - 1].index % 3}) with player ${
         positionWithPlayerHistory[move - 1].player
@@ -132,8 +148,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], path: lines[i] };
     }
   }
-  return null;
+  return { winner: null, path: null };
 }
